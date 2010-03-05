6a244e6e2a4c9a35061308a312023dd78fbfae08
Edson César <edsono@gmail.com>
2010-03-05 16:56:03 -0430

    Multiplatform get_sockname fake method

--- ftpd_spec.rb
+++ ftpd_spec.rb
@@ -24,7 +24,11 @@ class FTPServer
   # fake the socket data the server is running on. Resolves
   # to 127.0.0.1
   def get_sockname
-    "\002\000\000\025\177\000\000\001\000\000\000\000\000\000\000\000"
+    if RUBY_PLATFORM =~ /darwin/
+      "\020\002\010\111\177\000\000\001\000\000\000\000\000\000\000\000"
+    elsif RUBY_PLATFORM =~ /linux/
+      "\002\000\000\025\177\000\000\001\000\000\000\000\000\000\000\000"
+    end
   end
 
   def close_connection_after_writing
