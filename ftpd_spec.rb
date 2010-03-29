fadbea6ec2c08d5441a2138ded54032b20aa645c
James Healy <jimmy@deefa.com>
2010-03-29 19:58:08 +0530

    remove 2 specs that are no longer accurate
    * if LIST otr NLST are called with no data socket the server will
      keep trying until there is a data socket
    * thanks to Edson César for highlighting this

--- ftpd_spec.rb
+++ ftpd_spec.rb
@@ -280,15 +280,6 @@ context FTPServer, "LIST" do
     @c.sent_data.should match(/530.+/)
   end
 
-  specify "should respond with 150 ...425  when called with no data socket" do
-    @c = FTPServer.new(nil)
-    @c.receive_line("USER test")
-    @c.receive_line("PASS 1234")
-    @c.reset_sent!
-    @c.receive_line("LIST")
-    @c.sent_data.should match(/150.+425.+/m)
-  end
-
   specify "should respond with 150 ... 226 when called in the root dir with no param" do
     @c.receive_line("USER test")
     @c.receive_line("PASS 1234")
@@ -415,15 +406,6 @@ context FTPServer, "NLST" do
     @c.reset_sent!
     @c.receive_line("NLST")
     @c.sent_data.should match(/530.+/)
-  end
-
-  specify "should respond with 150 ...425  when called with no data socket" do
-    @c = FTPServer.new(nil)
-    @c.receive_line("USER test")
-    @c.receive_line("PASS 1234")
-    @c.reset_sent!
-    @c.receive_line("NLST")
-    @c.sent_data.should match(/150.+425.+/m)
   end
 
   specify "should respond with 150 ... 226 when called in the root dir with no param" do
