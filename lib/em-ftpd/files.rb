15768fb43e8bc52be4495815b5bdbe7948fa0e4c
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2013-04-19 08:22:09 +0900

    Implemented MDTM command(RFC3659). I expect to exist #mtime in driver.

--- lib/em-ftpd/files.rb
+++ lib/em-ftpd/files.rb
@@ -143,5 +143,21 @@ module EM::FTPD
       end
     end
 
+    # return the file's mtime
+    def cmd_mdtm(param)
+      send_unauthorised and return unless logged_in?
+      send_param_required and return if param.nil?
+
+      path = build_path(param)
+
+      @driver.mtime(path) do |result|
+        if result.kind_of?(Time)
+          send_response result.strftime("213 %Y%m%d%H%M%S%L")
+        else
+          send_response "550 file not available"
+        end
+      end
+    end
+
   end
 end
