8441f2a39fb07c3ce1a3f80a29eb626f7c6913f8
James Healy <james@yob.id.au>
2018-07-11 22:08:08 +1000

    enforce rspec 3.0 "expect" syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,8 @@ require 'em-ftpd'
 # in spec/support/ and its subdirectories.
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
 
-#RSpec.configure do |config|
-#  config.include ReaderSpecHelper
-#end
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+end
